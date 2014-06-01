b549ad3f0de5de76b2dff4ff78025c7e0dc88d9a
Klafyvel <klafyvel@gmail.com>
2014-06-01 17:48:25 +0200

    Added files to simplify GameWood initialsation.

---
+++ Librairies/GameWood/GameWood.h
@@ -0,0 +1,15 @@
+#ifndef H_GAMEWOOD
+#define H_GAMEWOOD
+
+#include "TVout.h"
+
+#define BT_UP           2
+#define BT_DOWN         12
+#define BT_LEFT         4
+#define BT_RIGHT        3
+#define BT_A            6
+#define BT_B            8
+
+void initGameWood();
+
+#endif
